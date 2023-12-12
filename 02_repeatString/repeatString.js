78ce31a0678f861b4206f35fc2d53122005c9f32
Christopher Nguyen <69370834+Christopher-NguyenCS@users.noreply.github.com>
2023-12-11 21:19:13 -0500

    Create function for repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(text, length) {
+    let newText = "";
+    if(length < 0){
+        return 'ERROR'
+    }
+    for (let i = 0; i < length; i++){
+        newText = newText + text;
+    }
+    return newText
 };
 
 // Do not edit below this line
